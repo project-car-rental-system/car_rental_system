87538298033c3f3076c0089f2bb8e6253a3d6c99
project-car-rental-system <waanienizar@gmail.com>
2025-06-22 15:09:33 +0800

    Update notification_data.dart

--- lib/data/notification_data.dart
+++ lib/data/notification_data.dart
@@ -2,22 +2,10 @@ import 'package:flutter/material.dart';
 
 List<Map<String, dynamic>> notifications = [
   {
-    'title': 'Return Reminder',
-    'message': 'Please return Myvi by 14 June before 5 PM.',
-    'icon': Icons.warning_amber_rounded,
-    'color': Colors.amber,
-  },
-  {
     'title': 'Special Promotion!',
     'message': 'Get 20% off all SUV bookings this week.',
     'icon': Icons.local_offer_rounded,
     'color': Colors.purple,
-  },
-  {
-    'title': 'Car Returned',
-    'message': 'Toyota Vios was returned successfully on 10 June.',
-    'icon': Icons.directions_car_filled,
-    'color': Colors.blue,
   },
 ];
 
